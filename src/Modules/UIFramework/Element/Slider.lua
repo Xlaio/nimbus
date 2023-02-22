59cee305c0231713ec3f1baaaae49de9c12853c0
Xlaio <Dev3dg@gmail.com>
2023-02-22 11:53:00 +0300

    V0.041

--- src/Modules/UIFramework/Element/Slider.lua
+++ src/Modules/UIFramework/Element/Slider.lua
@@ -8,12 +8,26 @@ function Slider.new(Properties,InitialPosition : number?, Minimum : number?, Max
     self.InitialPosition = InitialPosition or 0
     self.Minimum = Minimum or 0;
     self.Maximum = Maximum or 0
+
+    --#Elements maybe use the X:Createelement("Slider)
+    self.Container = Instance.new("Frame")
+    self.Line = Instance.new("Frame")
+    self.Slider = Instance.new("ImageButton")
+    self.Label = Instance.new("TextLabel")
+
     --# Events
     self.ChangedBindable = Instance.new("BindableEvent")
     self.DraggedBindable = Instance.new("BindableEvent")
     self.ReleasedBindable = Instance.new("BindableEvent")
     return self
 end
+
+--[[
+    TODO: Line Properties < Length based on Maximum, Height based on Propertie LineThichness
+    Container, Properties, would include Height, Width\
+    Label, Properties would include, The Text label size
+    Slider, Would include the Size : Heigh = Widthl, Using font Awesome images 
+]]
 
 function Slider:SetProperties(Properties : table)
     
@@ -33,6 +47,13 @@ function Slider:__index(key)
 end
 
 function Slider:Destroy()
+
+
+
+    self.ReleasedBindable:Disconnect()
+    self.DraggedBindable:Disconnect()
+    self.ChangeBindable:Disconnect()
+    setmetatable(self,{})
     --[[
         TODO: 
         Delete all connections
